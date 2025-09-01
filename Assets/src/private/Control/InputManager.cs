99d23e7ca7eed5df84a69a8ef20e91d1b62b82d7
amJohnnyma <dewaldcoleskydc@gmail.com>
2025-09-01 15:06:14 +0200

    Input manager with keybindings

---
+++ Assets/src/private/Control/InputManager.cs
@@ -0,0 +1,53 @@
+
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class InputBinding
+{
+    public string action;
+    public KeyCode key;
+}
+
+public class InputManager : MonoBehaviour
+{
+    public List<InputBinding> bindings;
+
+    public BuildMenu buildMenu;
+
+    void Update()
+    {
+        foreach (var binding in bindings)
+        {
+            if (Input.GetKeyDown(binding.key))
+            {
+                KeyPressAction(binding.action);
+            }
+        }
+    }
+
+    private void KeyPressAction(string action)
+    {
+
+        switch (action)
+        {
+            case "selectBO1":
+                buildMenu.PressButton(1);
+                break;
+            case "selectBO2":
+                buildMenu.PressButton(2);
+                break;
+            case "selectBO3":
+                buildMenu.PressButton(3);
+                break;
+            case "toggleBM": // build mode
+                buildMenu.ToggleBuildMode();
+                break;
+            case "toggleDM": // destroy mode
+                //buildMenu.ToggleBuildMode();
+                break;
+            
+        }
+
+    }
+}
--- Assets/src/private/UI/BuildMenu.cs
+++ Assets/src/private/UI/BuildMenu.cs
@@ -5,45 +5,26 @@ using UnityEngine.UIElements;
 
 public class BuildMenu : MonoBehaviour
 {
-    private UIDocument buildPanelDoc;
-    private Button btn1;
-    private Button btn2;
-    private Button btn3;
-
-    private Toggle toggleBuild;
-
     private bool buildMode = false;
 
     [SerializeField] private GameObject sphere;
+    [SerializeField] private UIDocument document;
     private Select select;
 
     private void Awake()
     {
         select = sphere.GetComponent<Select>();
-        buildPanelDoc = GetComponent<UIDocument>();
-
-        btn1 = buildPanelDoc.rootVisualElement.Q("BuildOption1") as Button;
-        btn1.RegisterCallback<ClickEvent>(evt => PressButton(evt, 1));
-
-        btn2 = buildPanelDoc.rootVisualElement.Q("BuildOption2") as Button;
-        btn2.RegisterCallback<ClickEvent>(evt => PressButton(evt, 2));
-
-        btn3 = buildPanelDoc.rootVisualElement.Q("BuildOption3") as Button;
-        btn3.RegisterCallback<ClickEvent>(evt => PressButton(evt, 3));
-
-        toggleBuild = buildPanelDoc.rootVisualElement.Q("BuildToggle") as Toggle;
-        toggleBuild.RegisterCallback<ClickEvent>(evt => ToggleBuildMode(evt));
-
-
+        
 
     }
+
 
     private void OnDisable()
     {
     }
 
 
-    private void PressButton(ClickEvent evt, int id)
+    public void PressButton(int id)
     {
         Debug.Log($"Option {id} clicked");
         select.SetBuildOption(buildMode, id);
@@ -51,7 +32,7 @@ public class BuildMenu : MonoBehaviour
 
     }
 
-    private void ToggleBuildMode(ClickEvent evt)
+    public void ToggleBuildMode()
     {
         buildMode = !buildMode;
         select.SetBuildOption(buildMode, -1);
